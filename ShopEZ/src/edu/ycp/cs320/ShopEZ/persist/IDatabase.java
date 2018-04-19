3c816315f2665fa5d26b034b907020195745812c
pmnorris <pnorris@ycp.edu>
2018-04-18 21:18:47 -0400

    Revert "Merge branch 'pmnorris'"
    This reverts commit aa5bc656b6938675dc9144c979ce6e2bfface3d7, reversing
    changes made to 1f7ea97261e0f70632bed8ab5449f67a3d563dda.

--- ShopEZ/src/edu/ycp/cs320/ShopEZ/persist/IDatabase.java
+++ ShopEZ/src/edu/ycp/cs320/ShopEZ/persist/IDatabase.java
@@ -2,6 +2,7 @@ package edu.ycp.cs320.ShopEZ.persist;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import edu.ycp.cs320.ShopEZ.model.Account;
 import edu.ycp.cs320.ShopEZ.model.Item;
@@ -14,9 +15,9 @@ public interface IDatabase {
 	public boolean verifyAccountFromAccountsTableByUsernameAndPassword(String username, String password) throws SQLException;
 	public double findItemPriceByItemName(String itemName) throws SQLException;
 	public int findAccountIDbyUsernameAndPassword(String username, String password) throws SQLException;
-	public Account findAccountByAccountID(final int id) throws SQLException;
+	public Account findAccountByAccountID(int id) throws SQLException;
 	public ArrayList<Account> findAllAccounts() throws SQLException;
-	public ArrayList<Item> findAllItemsForAccount(final int id) throws SQLException; 
+	public List<Item> findAllItemsForAccount(int id) throws SQLException; 
 	public Item updateItemPriceByItemNameAndPrice(final String name, final double price) throws SQLException;
 	public Item updateItemLocationByItemNameAndXYCoords(final String item, final int x, final int y) throws SQLException;
 	void dropTables() throws SQLException;
